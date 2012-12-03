c7dbee969539cc7cf7da3e37aac10d79c9749f3c
Jon Duell <jon@jonduell.com>
2012-12-02 16:47:06 -0800

    Splits format and wysiwyg tasks

--- lib/BaseProfile/Task/InputFormats.php
+++ lib/BaseProfile/Task/InputFormats.php
@@ -15,53 +15,19 @@ class InputFormats extends Task {
   protected $formats;
 
   /**
-   * Wysiwyg settings.
-   *
-   * @param array
-   */
-  protected $wysiwyg;
-
-  /**
-   * Default format/wysiwyg settings.
+   * Default format settings.
    */
   public function settings() {
-    $this->formats = $this->getFormatSettings();
-    $this->wysiwyg = $this->getWysiwygSettings();
+    $this->formats = $this->loadSettings('formats');
   }
 
   /**
    * Save format settings.
    */
   public function process() {
-    // Save formats.
     foreach ($this->formats as $format_name => $format) {
       $format = (object) $format;
       filter_format_save($format);
     }
-
-    // Save wysiwyg formats.
-    foreach ($this->wysiwyg as $wysiwyg_name => $object) {
-      db_insert('wysiwyg')
-        ->fields(array(
-          'format' => $object['format'],
-          'editor' => $object['editor'],
-          'settings' => serialize($object['settings']),
-        ))
-        ->execute();
-    }
-  }
-
-  /**
-   * Load up default format settings.
-   */
-  protected function getFormatSettings() {
-    return $this->loadSettings('formats');
-  }
-
-  /**
-   * Load up default wysiwyg settings.
-   */
-  protected function getWysiwygSettings() {
-    return $this->loadSettings('wysiwyg');
   }
 }
