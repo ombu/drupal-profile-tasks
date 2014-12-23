8c61a150875cbda290210e86d33106b559beeb67
Jon Duell <jon@jonduell.com>
2014-12-23 09:46:24 -0800

    Adds caption field to image Media elements

--- lib/ProfileTasks/Task/Media.php
+++ lib/ProfileTasks/Task/Media.php
@@ -65,9 +65,112 @@ class Media extends Task {
 
     // Default to "large" image style for default image file display.
     $displays = file_displays_load('image', 'default');
-    if (empty($displays['image__default__file_field_image']->settings['image_style'])) {
+    if (!isset($displays['image__default__file_field_image'])) {
+      $file_display = new \stdClass();
+      $file_display->api_version = 1;
+      $file_display->name = 'image__default__file_field_image';
+      $file_display->weight = -10;
+      $file_display->status = TRUE;
+      $file_display->settings = array(
+        'image_style' => 'large',
+      );
+      file_display_save($file_display);
+    }
+    elseif (empty($displays['image__default__file_field_image']->settings['image_style'])) {
       $displays['image__default__file_field_image']->settings['image_style'] = 'large';
       file_display_save($displays['image__default__file_field_image']);
+    }
+
+    $this->addCaptionField();
+  }
+
+  /**
+   * Adds a caption field to image file entities.
+   */
+  protected function addCaptionField() {
+    $base = array(
+      'active' => 1,
+      'cardinality' => 1,
+      'deleted' => 0,
+      'entity_types' => array(),
+      'field_name' => 'field_caption',
+      'foreign keys' => array(
+        'format' => array(
+          'columns' => array(
+            'format' => 'format',
+          ),
+          'table' => 'filter_format',
+        ),
+      ),
+      'indexes' => array(
+        'format' => array(
+          0 => 'format',
+        ),
+      ),
+      'locked' => 0,
+      'module' => 'text',
+      'settings' => array(),
+      'translatable' => 0,
+      'type' => 'text_long',
+    );
+    if (!field_info_field($base['field_name'])) {
+      field_create_field($base);
+    }
+
+    $instance = array(
+      'bundle' => 'image',
+      'default_value' => NULL,
+      'deleted' => 0,
+      'description' => '',
+      'display' => array(
+        'default' => array(
+          'label' => 'hidden',
+          'module' => 'text',
+          'settings' => array(),
+          'type' => 'text_default',
+          'weight' => 1,
+        ),
+        'preview' => array(
+          'label' => 'above',
+          'settings' => array(),
+          'type' => 'hidden',
+          'weight' => 0,
+        ),
+        'teaser' => array(
+          'label' => 'above',
+          'settings' => array(),
+          'type' => 'hidden',
+          'weight' => 0,
+        ),
+        'wysiwyg' => array(
+          'label' => 'above',
+          'settings' => array(),
+          'type' => 'hidden',
+          'weight' => 0,
+        ),
+      ),
+      'entity_type' => 'file',
+      'field_name' => 'field_caption',
+      'label' => 'Caption',
+      'required' => 0,
+      'settings' => array(
+        'text_processing' => 0,
+        'user_register_form' => FALSE,
+        'wysiwyg_override' => 1,
+      ),
+      'widget' => array(
+        'active' => 1,
+        'module' => 'text',
+        'settings' => array(
+          'rows' => 5,
+        ),
+        'type' => 'text_textarea',
+        'weight' => 4,
+      ),
+      'workbench_access_field' => 0,
+    );
+    if (!field_info_instance($instance['entity_type'], $instance['field_name'], $instance['bundle'])) {
+      field_create_instance($instance);
     }
   }
 }
